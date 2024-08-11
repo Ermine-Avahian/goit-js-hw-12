2953ceeb72f62a1f989f555817ba991385278179
Ermine <avahian.hem@gmail.com>
2024-08-11 20:55:30 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,9 @@ export default defineConfig(({ command }) => {
       },
       outDir: '../dist',
     },
+    optimizeDeps: {
+      exclude: ['axios'],
+    },
     plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
   };
 });
